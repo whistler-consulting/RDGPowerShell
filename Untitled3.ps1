a29ccd3a0fd3c4c2f59b0f72cbb18f99acaa16ff
ms_mjenne@hotmail.com <ms_mjenne@hotmail.com>
2016-09-14 00:39:31 -0500

    before conversion to cim
    kkk

---
+++ Untitled3.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module C:\Users\MikeJenne\Documents\GitHub\RDGPowerShell\RDGModule.psm1 -Verbose -Force
+
+if (Test-Path Variable:Global:RDGCredential) { Remove-Variable -Name RDGCredential }
+
+
+New-RDGCredential -UserName jenne\mjenne
+
+New-RDGResourceGroup -Name RG01 -Description "RG01 Description" -ComputerName 10.0.0.208 -Resources "10.0.0.201"
+
+New-RDGRAP -Name "RAP01" -Description "RAP01 Description" -ResourceGroupName "RG01" -ResourceGroupType RG -UserGroupNames "jenne\domain users" -ComputerName 10.0.0.208
+
+New-RDGRAP -Name RAP02 -Description "RAP02 Description" -ResourceGroupName "RG01" -ResourceGroupType RG -UserGroupNames "jenne\domain users" -ComputerName 10.0.0.208
+
+Add-RDGResource -ComputerName 10.0.0.208 -Name RG01 -Resource 10.0.0.101 -Verbose
+
+Add-RDGResource -ComputerName 10.0.0.208 -Name RG02 -Resource 10.0.0.100 -Verbose
+
+Get-RDGResourceAuthorizationPolicy -ComputerName 10.0.0.208 -Name RAP01
+
+Get-RDGResourceAuthorizationPolicy -ComputerName 10.0.0.208 | ft name
+
+
+
+
+
+
+
+
+
+
---
+++ Untitled6.ps1
@@ -0,0 +1,17 @@
+﻿$co = New-CimSessionOption -Impersonation Impersonate -PacketIntegrity -PacketPrivacy 
+
+$cs = New-CimSession -ComputerName 10.0.0.208 -Credential (Get-Credential jenne\mjenne) -SessionOption $co -Authentication NtlmDomain
+
+
+#Get-CimInstance -CimSession $cs -ClassName "Win32_operatingsystem" -Verbose
+
+Get-CimClass -CimSession $cs -ClassName "Win32_TSGatewayConnectionAuthorizationPolicy" -Namespace "Root\CIMv2\TerminalServices" -Verbose
+
+Get-CimInstance -CimSession $cs -ClassName "Win32_TSGatewayConnectionAuthorizationPolicy" -Namespace "Root\CIMv2\TerminalServices" -Verbose
+
+#Get-CimClass -CimSession $cs -Namespace "Root\CIMv2\TerminalServices" 
+
+
+
+
+
